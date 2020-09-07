91454cdfeeb428369a29efa836e0734733c3751c
KianRabie <70865307+KianRabie@users.noreply.github.com>
2020-09-06 22:25:19 -0700

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Number_Guesser
+{
+    public class Class1
+    {
+        public static void Main()
+        {
+            //Generates random number
+            
+            Random rnd = new Random();
+            int rdmNum = rnd.Next(100);
+            int guesses = 1;
+
+            //Asks for user to input guess
+
+            int guess;
+            Console.WriteLine("Guess the random number!");
+            var guessIn = Console.ReadLine();
+
+            //Checks input is a number
+
+            bool success = Int32.TryParse(guessIn, out guess);
+            if (success)
+            {
+                if (guess > rdmNum)
+                {
+                    Console.WriteLine("Lower!");
+                }
+
+                if (guess < rdmNum)
+                {
+                    Console.WriteLine("Higher!");
+                }
+            }
+            else Console.WriteLine("Feed me numbers!");
+
+            //Checks if the guess was higher or lower
+
+
+
+            //Loops until answer is guessed
+
+            while (guess != rdmNum)
+            {
+                Console.WriteLine("Guess again!");
+
+                guessIn = Console.ReadLine();
+
+             //takes input and sees if its either higher or lower or exact
+             //tells user if it was higher lower or exact
+
+                success = Int32.TryParse(guessIn, out guess);
+                if (success) ;
+                else Console.WriteLine("Feed me numbers!");
+
+                if (guess > rdmNum)
+                {
+                    Console.WriteLine("Lower!");
+                }
+
+                if (guess < rdmNum)
+                {
+                    Console.WriteLine("Higher!");
+                }
+
+                guesses ++;
+            }
+
+            //prints number of attempts once number is guessed
+
+            if (guess == rdmNum)
+            {
+                Console.WriteLine("You got it! You guessed " +guesses+ " times!");
+            }
+
+        }
+       
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Xml;
+
+//User inputs string
+//String is split into floats
+//Checks what strings are floats or operators
+//Input values into specified operator formula
+//Outputs result
+
+class Calculator
+{
+    public static void Main()
+    {
+        //Asks for math
+        Console.WriteLine("Input MATH:");
+
+        //Reads input
+        string userInput = Console.ReadLine();
+
+        //Splits string
+        string[] inputList = userInput.Split(' ');
+
+        var inputNum  = inputList[0];
+        var operation = inputList[1];
+        var inputNum2 = inputList[2];
+
+        //Makes sure theres a valid amount of strings
+        if (inputList.Length < 3)
+        {
+            Console.WriteLine("PLEASE adhere to # operator # format, thanks");
+            Console.WriteLine();
+        }
+        //Turns split strings into floats
+        float num1, num2;
+        float.TryParse(inputNum, out num1); 
+        float.TryParse(inputNum2, out num2);
+
+        //Figures out what operator "operation" is
+        float result;
+        switch (operation)
+        {
+            case "+":
+                result = num1 + num2;
+                Console.WriteLine($"The answer is {result}");
+                break;
+                
+            case "-":
+                result = num1 - num2;
+                Console.WriteLine($"The answer is {result}");
+                break;
+
+            case "*":
+                result = num1 * num2;
+                Console.WriteLine($"The answer is {result}");
+                break;
+
+            case "/":
+                result = num1 / num2;
+                Console.WriteLine($"The answer is {result}");
+                break;
+        }
+        
+    }
+}
+ 
